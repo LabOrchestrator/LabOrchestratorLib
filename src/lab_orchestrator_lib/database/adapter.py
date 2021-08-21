e8f5fa3131327edf8dc4643ae6abfe8e543d0712
Marco Schlicht <git@privacymail.dev>
2021-08-21 14:56:47 +0200

    added more type hints to adapter

--- src/lab_orchestrator_lib/database/adapter.py
+++ src/lab_orchestrator_lib/database/adapter.py
@@ -1,72 +1,73 @@
-from typing import List
+from typing import List, Any, Dict
 
-from lab_orchestrator_lib.model.model import DockerImage, Lab, LabInstance
+from lab_orchestrator_lib.model.model import DockerImage, Lab, LabInstance, Identifier
 
 
-class DockerImageAdapter:
-    def create(self, obj: DockerImage) -> DockerImage:
+class DockerImageAdapterInterface:
+    def create(self, name: str, description: str, url: str) -> DockerImage:
         raise NotImplementedError()
 
     def get_all(self) -> List[DockerImage]:
         raise NotImplementedError()
 
-    def get(self, identifier) -> DockerImage:
+    def get(self, identifier: Identifier) -> DockerImage:
         raise NotImplementedError()
 
-    def get_by_attr(self, attr, value) -> DockerImage:
+    def get_by_attr(self, attr: str, value: Any) -> DockerImage:
         raise NotImplementedError()
 
-    def delete(self, identifier) -> None:
+    def delete(self, identifier: Identifier) -> None:
         raise NotImplementedError()
 
     def save(self, obj: DockerImage) -> DockerImage:
         raise NotImplementedError()
 
-    def filter(self, **kwargs) -> DockerImage:
+    def filter(self, **kwargs: Dict[str, Any]) -> DockerImage:
         raise NotImplementedError()
 
 
-class LabAdapter:
-    def create(self, obj: Lab) -> Lab:
+class LabAdapterInterface:
+    def create(self, name: str, namespace_prefix: str, description: str, docker_image_id: Identifier,
+               docker_image_name: str) -> Lab:
         raise NotImplementedError()
 
     def get_all(self) -> List[Lab]:
         raise NotImplementedError()
 
-    def get(self, identifier) -> Lab:
+    def get(self, identifier: Identifier) -> Lab:
         raise NotImplementedError()
 
-    def get_by_attr(self, attr, value) -> Lab:
+    def get_by_attr(self, attr: str, value: Any) -> Lab:
         raise NotImplementedError()
 
-    def delete(self, identifier) -> None:
+    def delete(self, identifier: Identifier) -> None:
         raise NotImplementedError()
 
     def save(self, obj: Lab) -> Lab:
         raise NotImplementedError()
 
-    def filter(self, **kwargs) -> Lab:
+    def filter(self, **kwargs: Dict[str, Any]) -> Lab:
         raise NotImplementedError()
 
 
-class LabInstanceAdapter:
-    def create(self, obj: LabInstance) -> LabInstance:
+class LabInstanceAdapterInterface:
+    def create(self, lab_id: Identifier, user_id: Identifier) -> LabInstance:
         raise NotImplementedError()
 
     def get_all(self) -> List[LabInstance]:
         raise NotImplementedError()
 
-    def get(self, identifier) -> LabInstance:
+    def get(self, identifier: Identifier) -> LabInstance:
         raise NotImplementedError()
 
-    def get_by_attr(self, attr, value) -> LabInstance:
+    def get_by_attr(self, attr: str, value: Any) -> LabInstance:
         raise NotImplementedError()
 
-    def delete(self, identifier) -> None:
+    def delete(self, identifier: Identifier) -> None:
         raise NotImplementedError()
 
     def save(self, obj: LabInstance) -> LabInstance:
         raise NotImplementedError()
 
-    def filter(self, **kwargs) -> LabInstance:
+    def filter(self, **kwargs: Dict[str, Any]) -> LabInstance:
         raise NotImplementedError()
