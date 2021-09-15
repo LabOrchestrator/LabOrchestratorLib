300b662f7cf16f9736725d6fa2ec16a64b20945b
Marco Schlicht <git@privacymail.dev>
2021-09-15 14:41:48 +0200

    added clear registry method to mockup

--- tests/controller/mockup.py
+++ tests/controller/mockup.py
@@ -1,7 +1,7 @@
 import unittest
 from typing import Tuple
 
-from lab_orchestrator_lib.kubernetes.api import APIRegistry
+from lab_orchestrator_lib.kubernetes.api import APIRegistry, _API_EXTENSIONS_NOT_NAMESPACED, _API_EXTENSIONS_NAMESPACED
 from tests.kubernetes.mockups import ProxyMock
 
 
@@ -10,3 +10,8 @@ def get_mocked_registry(test_case: unittest.TestCase) -> Tuple[ProxyMock, APIReg
     proxy.test = test_case
     registry = APIRegistry(proxy)
     return proxy, registry
+
+
+def clear_registry():
+    _API_EXTENSIONS_NAMESPACED = {}
+    _API_EXTENSIONS_NOT_NAMESPACED = {}
